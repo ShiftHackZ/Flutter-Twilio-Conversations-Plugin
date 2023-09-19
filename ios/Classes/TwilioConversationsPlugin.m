250fa180394c5227099e464db9c2d88361856fc7
ShiftHackZ <dmitriy@moroz.cc>
2023-09-19 13:35:00 +0300

    Fix 1

--- ios/Classes/TwilioConversationsPlugin.m
+++ ios/Classes/TwilioConversationsPlugin.m
@@ -1,11 +1,11 @@
 #import "TwilioConversationsPlugin.h"
-#if __has_include(<twilio_conversations/twilio_conversations-Swift.h>)
+#if __has_include(<flutter_twilio_chat_conversations/flutter_twilio_chat_conversations-Swift.h>)
 #import <twilio_conversations/twilio_conversations-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "twilio_conversations-Swift.h"
+#import "flutter_twilio_chat_conversations-Swift.h"
 #endif
 
 @implementation TwilioConversationsPlugin
